3049e94789cad93f4589aa51d575e9c405cf0a8f
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2025-04-11 20:43:46 +0200

    feat(interfaces): Añadir default de engage y disengage con cantidad

--- src/main/java/edu/estatuas/interfaces/Heater.java
+++ src/main/java/edu/estatuas/interfaces/Heater.java
@@ -5,4 +5,10 @@ import edu.estatuas.types.RoomTemperature;
 public interface Heater {
     void engage(RoomTemperature temperature);
     void disengage(RoomTemperature temperature);
+    default void engage(RoomTemperature temperature, double increment) {
+        engage(temperature);
+    }
+    default void disengage(RoomTemperature temperature, double decrement) {
+        disengage(temperature);
+    }
 }
