f79d1e111c58becf0d455872010504e184be2b4a
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2025-04-11 18:35:10 +0200

    feat(interfaces): Añadir interfacs Heatr y Thermometer

---
+++ src/main/java/edu/estatuas/interfaces/Heater.java
@@ -0,0 +1,6 @@
+package edu.estatuas.interfaces;
+
+public interface Heater {
+    void engage();
+    void disengage();
+}
---
+++ src/main/java/edu/estatuas/interfaces/Thermometer.java
@@ -0,0 +1,5 @@
+package edu.estatuas.interfaces;
+
+public interface Thermometer {
+    void read();
+}
