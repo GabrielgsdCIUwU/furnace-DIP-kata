6d2f575b8e770c5fcc9bae85f6c22c0e73664103
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2025-04-11 20:44:17 +0200

    feat(hardware): Añadir en GasHeater la cantidad de incremento

--- src/main/java/edu/estatuas/hardware/GasHeater.java
+++ src/main/java/edu/estatuas/hardware/GasHeater.java
@@ -11,7 +11,17 @@ public class GasHeater implements Heater {
     }
 
     @Override
+    public void engage(RoomTemperature temperature, double increment) {
+        temperature.incrementTemperature(increment);
+    }
+
+    @Override
     public void disengage(RoomTemperature temperature) {
         temperature.decrementTemperature(1);
+    }
+
+    @Override
+    public void disengage(RoomTemperature temperature, double decrement) {
+        temperature.decrementTemperature(decrement);
     }
 }
